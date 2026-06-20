f95fb1a1b17b11691d0249ebc78ca63a1c2edadb
m7nter <gz796wvgy5@privaterelay.appleid.com>
2026-06-21 01:03:25 +0300

    Update SecureVaultApp.swift

--- Sources/SecureVault/App/SecureVaultApp.swift
+++ Sources/SecureVault/App/SecureVaultApp.swift
@@ -6,16 +6,13 @@ struct SecureVaultApp: App {
 
     var body: some Scene {
         WindowGroup {
-            Group {
-                if isUnlocked {
-                    VaultView()
-                } else {
-                    CalculatorView(onUnlock: {
-                        isUnlocked = true
-                    })
-                }
+            if isUnlocked {
+                VaultView()
+            } else {
+                CalculatorView(onUnlock: {
+                    isUnlocked = true
+                })
             }
-            .preferredColorScheme(.dark)
         }
     }
 }
